95d9dd29f7e6809d2c2fb1c690bddb1aad64aa4c
Salman <salmanjamal2003@gmail.com>
2024-06-06 17:19:35 +0530

    between-eloquent

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,8 +14,7 @@ class UserController extends Controller
             $query->where('status', 1)
                 ->orWhere(function ($query) {
                     $query->where(function ($query) {
-                        $query->where('km', '<', 0)
-                            ->orWhere('km', '>', 200);
+                        $query->whereBetween('km',['1','200']);
                     })->where('role', 1);
                 });
         })->where('age', '>', 20)->get();
